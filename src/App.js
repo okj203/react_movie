bc50746d1e6925dbbd74c59956c2181af70fa01c
okj203 <okj203@gmail.com>
2021-07-26 17:41:39 -0400

    movie component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  // add = () => { 
+  //   // relying on the state directly is an anti-pattern
+  //   // `current` is equivalent to `this.state`
+  //   this.setState(current => ({ count: current.count + 1})) 
+  // };
+  // minus = () => { this.setState({ count: this.state.count - 1}) };
+
+  // can use either `fetch` or `axios` to fetch data
+  // We need to grab the data that comes from the axios request so we can use it in our state
+  // axios.get() is not always fast -> We need to tell JS that componentDidMount() might take time to finish so we want to wait for it
+
+  // async-await: indicate a function is an async and await for the axios request to get back with a response
+  // await cannot be used outside async
+  getMovies = async () => {
+    const { 
+      data: { 
+        data: { movies } 
+      } 
+    } = await axios.get(
+      // "https://yts-proxy.nomadcoders1.now.sh/list_movies.json" //?sort_by=rating -> causes error
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating" 
+      );
+    // console.log(movies);
+    this.setState({ movies, isLoading: false });
+    // await fetch("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+    //   .then(response => response.json())
+    //   .then(data => console.log(data));
+  }
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render(){
+    // setState triggers re-render
+    const { isLoading, movies } = this.state;
+    return (
+      <>
+        <section className="container">
+          { isLoading ? (
+            <div>
+              <span className="loader__text">Loading...</span>
+            </div>
+          ) : (
+            <div className="movies">
+              {movies.map(movie => {
+              // console.log(movie);
+                return (
+                  <Movie 
+                    key={movie.id}
+                    id={movie.id} 
+                    year={movie.year} 
+                    title={movie.title} 
+                    summary={movie.summary} 
+                    poster={movie.medium_cover_image}
+                  />
+              )})}
+            </div>
+            )
+          }
+        </section>
+      </>
+    )  
+  }
 }
 
 export default App;
+
+// component - a function that returns html
+// jsx - js + html
